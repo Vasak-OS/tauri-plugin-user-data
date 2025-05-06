6bac2831fe0ba93c6bde293f26c19ebd5e3888d7
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-05-06 17:22:37 -0300

    Fix formatting of invoke type parameter in getUserData function

--- guest-js/index.ts
+++ guest-js/index.ts
@@ -1,7 +1,7 @@
 import { invoke } from '@tauri-apps/api/core'
 
 export async function getUserData(): Promise<UserInfo | null> {
-  return await invoke< UserInfo >('plugin:user-data|get_user_info')
+  return await invoke<UserInfo>('plugin:user-data|get_user_info')
     .then((r) => (r ? r : null));
 }
 
